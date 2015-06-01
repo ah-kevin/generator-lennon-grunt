418683d11accad9b361f9723a8eaeb1bfcff28d6
mac-Leoneo <kbjwdh@gmail.com>
2015-06-01 11:35:48 +0800

    1.0.1

--- app/index.js
+++ app/index.js
@@ -9,17 +9,6 @@ module.exports = yeoman.generators.Base.extend({
   constructor: function () {
     yeoman.generators.Base.apply(this, arguments);
 
-
-
-    this.option('skip-install', {
-      desc: 'Skips the installation of dependencies',
-      type: Boolean
-    });
-
-    this.option('skip-install-message', {
-      desc: 'Skips the message after the installation of dependencies',
-      type: Boolean
-    });
     this.pkg = require('../package.json');
   },
 
@@ -118,8 +107,10 @@ module.exports = yeoman.generators.Base.extend({
   },
 
   writeIndex: function () {
-    this.indexFile = this.src.read('index.html');
-    this.indexFile = this.engine(this.indexFile, this);
+    this.indexFile = this.engine(
+        this.readFileAsString(join(this.sourceRoot(), 'index.html')),
+        this
+    );
 
     // wire Bootstrap plugins
     if (this.includeBootstrap && !this.includeSass) {
@@ -166,56 +157,15 @@ module.exports = yeoman.generators.Base.extend({
     this.copy('main.js', 'app/scripts/main.js');
 
   },
-
   install: function () {
-
-    var howToInstall =
-        '\nAfter running ' +
-        chalk.yellow.bold('npm install & bower install') +
-        ', inject your' +
-        '\nfront end dependencies by running ' +
-        chalk.yellow.bold('grunt wiredep') +
-        '.';
-
-    if (this.options['skip-install']) {
-        this.log(howToInstall);
-         return;
-    }
-    this.installDependencies({
-      skipMessage: this.options['skip-install-message'],
-      skipInstall: this.options['skip-install']
-    });
-
     this.on('end', function () {
-      var bowerJson = this.dest.readJSON('bower.json');
 
-      // wire Bower packages to .html
-      wiredep({
-        bowerJson: bowerJson,
-        directory: 'bower_components',
-        exclude: ['bootstrap-sass', 'bootstrap.js'],
-        ignorePath: /^(\.\.\/)*\.\./,
-        src: 'app/index.html'
-      });
-
-      if (this.includeSass) {
-        // wire Bower packages to .scss
-        wiredep({
-          bowerJson: bowerJson,
-          directory: 'bower_components',
-          ignorePath: /^(\.\.\/)+/,
-          src: 'app/styles/*.scss'
+      if (!this.options['skip-install']) {
+        this.installDependencies({
+          skipMessage: this.options['skip-install-message'],
+          skipInstall: this.options['skip-install']
         });
       }
-      // ideally we should use composeWith, but we're invoking it here
-      // because generator-mocha is changing the working directory
-      // https://github.com/yeoman/generator-mocha/issues/28
-      //this.invoke(this.options['test-framework'], {
-      //  options: {
-      //    'skip-message': this.options['skip-install-message'],
-      //    'skip-install': this.options['skip-install']
-      //  }
-      //});
-    }.bind(this));
+    });
   }
 });
