ce7ae6fea3baf55c78c63b218a33b4cb083df69f
mac-Leoneo <kbjwdh@gmail.com>
2015-06-01 09:27:19 +0800

    fix

--- app/index.js
+++ app/index.js
@@ -7,7 +7,6 @@ module.exports = yeoman.generators.Base.extend({
   constructor: function () {
     yeoman.generators.Base.apply(this, arguments);
 
-
     this.pkg = require('../package.json');
   },
 
